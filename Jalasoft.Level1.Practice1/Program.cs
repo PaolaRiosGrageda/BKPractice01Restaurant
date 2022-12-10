337c42b893ea95c3477dc38a005cd2b252d97db0
Paola Rios <Paola.Rios@jala.university>
2022-12-09 20:34:29 -0400

    add menu

---
+++ Jalasoft.Level1.Practice1/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Jalasoft.Level1.Practice1;
+
+public struct Program
+{
+    static void Main(string[] args)
+    {
+        RestaurantMenu menu1 = new RestaurantMenu("Practice 01 Restaurant");
+
+        Console.WriteLine("Hello, World!");
+        menu1.Start();
+    }
+    
+
+}
+
---
+++ Jalasoft.Level1.Practice1/RestaurantMenu.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Jalasoft.Level1.Practice1
+{
+    public struct RestaurantMenu
+    {
+        private string nameRestaurant;
+
+        private int exitOption = 0;
+        public RestaurantMenu(string name) { 
+        
+            this.nameRestaurant = name;
+        }
+        private int ChooseOption()
+        {
+            this.ShowOptions();
+            string option= Console.ReadLine();
+            int selection = int.Parse(option);  
+            return selection; 
+        }
+        private void ShowOptions()
+        {
+            Console.WriteLine("");
+            Console.WriteLine("--------------");
+            Console.WriteLine("1. Register Customer");
+            Console.WriteLine("2. Show Menu");
+            Console.WriteLine("3. Register order ");
+            Console.WriteLine("4. Show debt");
+            Console.WriteLine("0. Exit");
+        }
+        public void Start()
+        {
+            int option;
+            do
+            {
+                option= ChooseOption();
+                ExecuteOption(option);
+            }while (option!= exitOption);
+             
+           
+
+        }
+        private void ExecuteOption(int option)
+        {
+            switch(option)
+            {
+                case 1:
+                    Console.WriteLine("usted seleciono uno");
+                    break;
+                case 2:
+                    Console.WriteLine("usted seleciono2");
+                    break;
+                case 3: Console.WriteLine("Usted selecciono3");
+                    break;
+                case 4:
+                    Console.WriteLine("Usted selecciono4");
+                    break;
+
+                case 0:
+                    Console.WriteLine("Usted selecciono0");
+                    break;
+                default:
+                    break;
+            }
+
+        }
+
+
+
+    }
+}
